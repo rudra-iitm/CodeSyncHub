9c3b70f2252042cfd35290a9f3c2131c6675cb65
Shivam Jaiswal <shivam181106@gmail.com>
2024-02-26 19:00:50 +0530

    frontend1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
+// import React, { useRef, useEffect } from 'react';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className='text-3xl font-bold underline text-center'>CodeSyncHub</h1>
-    </>
+    // <></>
+    // <div className=' bg-red-500 h-lvh w-lvw overflow-hidden'>
+    //   <h1 className='text-center'>sdfgh</h1>
+    // </div>
+
+    // <div className="bg-teal-200 h-lvh w-lvh">
+    <div className='h-lvh w-lvh bg-gradient-to-r from-black via-green-600 to-pink-500' style={{background: "linear-gradient(to right,black, white)"}}>
+    <h1 className='text-3xl font-bold underline text-center pt-5'>CodeSyncHub</h1>
+    </div>
+    // </div>
   )
 }
 
