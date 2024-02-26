4b3c286de46ebd5864d6568eb8a5fb9b10dab927
sushant-wayal <sushantssr12345@gmail.com>
2024-02-26 20:37:21 +0530

    textAnimate sw

--- frontend/src/Components/Home.tsx
+++ frontend/src/Components/Home.tsx
@@ -1,10 +1,51 @@
+import { useEffect } from "react"
+
 const Home = () => {
+    useEffect(() => {
+        let inner = document.querySelector("#inner");
+        let innerRect = inner?.getBoundingClientRect();
+        let innerHeight = innerRect?.bottom-innerRect?.top;
+        let text1 = document.querySelector("#text1");
+        let text2 = document.querySelector("#text2");
+        let content = document.querySelector("#content");
+        let k = 20;
+        const textAnimate = () => {
+            text1.style.fontSize = (60+(window.scrollY/(k)))+"px";
+            text2.style.fontSize = (24+(window.scrollY/(k)))+"px";
+            content.style.opacity = 1-(window.scrollY/(k*15));
+            if (window.scrollY >= 10) {
+                document.querySelector("#start").style.opacity = 0;
+            } else {
+                document.querySelector("#start").style.opacity = 1;
+            }
+            if (window.scrollY >= 0 && window.scrollY <= innerHeight) {
+                inner.style.position = "fixed";
+                inner.style.top = "0px";
+            }
+            else if (window.scrollY >= innerHeight) {
+                inner.style.position = "relative";
+                inner.style.top = "100vh";
+            }
+            else {
+                inner.style.position = "relative";
+                inner.style.top = "0px";
+            }
+        }
+        document.addEventListener("scroll", textAnimate);
+        return () => document.removeEventListener("scroll",textAnimate);
+    },[])
     return (
-        <div>
-            <div>
-                <p></p>
-                <p></p>
-                <button></button>
+        <div className="overflow-x-hidden">
+            <div id="outer" className="h-[200vh] w-lvw relative">
+                <div id="inner" className="h-lvh w-lvw flex justify-center items-center">
+                    <div id="content" className="flex  flex-col gap-7 items-center">
+                        <p id="text1" className="text-6xl">Idea to software, fast</p>
+                        <p id="text2" className="text-2xl text-center">Build software collaboratively with the power of AI, on any 
+                        <br/>
+                        device, without spending a second on setup</p>
+                        <button id="start" style={{boxShadow: "1px 1px 20px blue, -1px -1px 20px blue"}} className="py-3 px-2 text-lg font-semibold rounded-2xl">Start Creating</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
