3e763fdf21e046d2155da82f2765129a1189d4dc
Shivam Jaiswal <shivam181106@gmail.com>
2024-02-26 20:51:42 +0530

    frontend3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
-import './App.css'
 import Home from './Components/Home'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Layout from './Components/Layout';
 
 function App() {
   return (
-    <Home/>
+    <BrowserRouter>
+    <Routes>
+     <Route path="/" element={<Layout/>}>
+       <Route index element={<Home/>} />
+         {/* <Route path="todo" element={<Todo/>} />
+         <Route path="sign-up" element={<Register/>} />
+         <Route path="sign-in" element={!access_token ? <Login /> : <Navigate to="/dashboard" />} />
+         <Route path="sent-reset-link" element={<ForgetPass/>} /> */}
+       </Route>
+    </Routes>
+   </BrowserRouter>
   )
 }
 
