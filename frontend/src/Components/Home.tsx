a53fff1bbb96a9ec5e8b3945ccdd0fbae453d26e
sushant-wayal <sushantssr12345@gmail.com>
2024-02-26 19:09:37 +0530

    frontend_sw

---
+++ frontend/src/Components/Home.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <p></p>
+                <p></p>
+                <button></button>
+            </div>
+        </div>
+    )
+}
+
+export default Home
