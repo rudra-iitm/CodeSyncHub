da530a34537a17e69522444c3f03992616e83fad
sushant-wayal <sushantssr12345@gmail.com>
2024-02-28 22:14:58 +0530

    fixed error sw

--- frontend/src/Components/CodeEditor.tsx
+++ frontend/src/Components/CodeEditor.tsx
@@ -1,17 +1,6 @@
-import { useRef, useEffect } from 'react';
-import $ from 'jquery';
-import 'jquery-ui-dist/jquery-ui'; // Import jQuery UI
-
 const CodeEditor = () => {
-    const resizableDivRef = useRef(null);
-
-    useEffect(() => {
-        if (resizableDivRef.current) {
-        $(resizableDivRef.current).resizable(); // Initialize resizable
-        }
-    }, []);
     return (
-        <div ref={resizableDivRef} className="w-3/5 flex-grow bg-blue-600"></div>
+        <div className="w-3/5 flex-grow bg-blue-600"></div>
     )
 }
 
