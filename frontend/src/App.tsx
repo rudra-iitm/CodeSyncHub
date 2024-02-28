9f0e36bc9974cf0743ae52d98d56ecb827d82825
Shivam Jaiswal <shivam181106@gmail.com>
2024-02-28 19:51:27 +0530

    frontend_6

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import Home from './Components/Home'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from './Components/Layout';
 import Editor from './Components/Editor';
+import Register from './Components/Register';
 
 function App() {
   return (
@@ -9,12 +10,13 @@ function App() {
     <Routes>
      <Route path="/" element={<Layout/>}>
        <Route index element={<Home/>} />
+       <Route path="register" element={<Register/>} />
          {/* <Route path="todo" element={<Todo/>} />
-         <Route path="sign-up" element={<Register/>} />
          <Route path="sign-in" element={!access_token ? <Login /> : <Navigate to="/dashboard" />} />
         <Route path="sent-reset-link" element={<ForgetPass/>} /> */}
        </Route>
       <Route path="/editor" element={<Editor/>} />
+      <Route path="*" element={<h1>Error 404 Page not found !!</h1>} />
     </Routes>
    </BrowserRouter>
   )
